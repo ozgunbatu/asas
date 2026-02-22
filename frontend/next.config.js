3f062b452e7d91308e4cdab01ca4cd43f70967d1
Ozgun Batuhan Dogan <ozgunbatuhan@gmail.com>
2026-02-22 23:35:05 +0100

    Fix syntax for Next.js config file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
+  typescript: { ignoreBuildErrors: true },
 };
 
 module.exports = nextConfig;
